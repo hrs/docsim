e36485ee13b673686114d8a1cb189326dcd09f9e
Harry R. Schwartz <hello@harryrschwartz.com>
2023-05-12 15:00:14 -0700

    Get target file and search files from args

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
 
-func main () {
-	  fmt.Println("hello!")
+func main() {
+	targetFlag := flag.String("target", "", "path to the file that results should match")
+	flag.Parse()
+
+	docs := flag.Args()
+
+	fmt.Println("target: ", *targetFlag)
+	fmt.Println("corpus:", docs)
 }
